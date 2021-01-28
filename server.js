5acb2e1866b85ce33a211241465a8978ebcb1ad0
jleatham78 <janeleleatham01@gmail.com>
2021-01-27 22:52:50 -0700

    added script files

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
+const { response } = require('express');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 
 
@@ -96,6 +99,18 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
 
   
 app.listen(PORT, () => {
